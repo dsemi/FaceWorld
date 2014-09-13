7d5c7159199a34f6fc160412831e7f24f6bda1a5
Dan Seminara <semi225599@gmail.com>
2014-09-13 15:10:13 -0400

    Amazon

---
+++ application.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
