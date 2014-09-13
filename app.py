56d01f4efb5f12d571f3683702541f62693c585d
Dan Seminara <semi225599@gmail.com>
2014-09-13 15:45:31 -0400

    Work herokyu

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +10,7 @@ def root():
     return 'Hello World'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    if port == 5000:
+        app.debug = True
+    app.run(host='0.0.0.0', port=port)
