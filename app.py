9cdac509565fb3fc0711b9e10e7e13bc2ebdaee9
Dan Seminara <semi225599@gmail.com>
2014-09-13 15:04:26 -0400

    Amazon test

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python2
 
-'''
-
-Usage: app.py [--debug] [-p <port>] [-H <host>]
-
-Options:
-  -p, --port=<port>   The port number on which to run Flask [default: 5000]
-  -H, --host=<host>   The host to listen to [default: 127.0.0.1]
-  --debug             Flag to determine debug mode [default: False]
-
-'''
-
 from flask import Flask
-from docopt import docopt
-from schema import Use, Schema
 
 app = Flask(__name__)
 
@@ -22,5 +9,4 @@ def root():
     return 'Hello World'
 
 if __name__ == '__main__':
-    args = Schema({'--host': Use(str), '--port': Use(int), '--debug': Use(bool)}).validate(docopt(__doc__))
-    app.run(host=args['--host'], port=args['--port'], debug=args['--debug'])
+    app.run(host='0.0.0.0', port=80, debug=True)
