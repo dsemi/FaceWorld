9cffa62cb1b7c487a44a885e9cc5c2b525f81f60
Dan Seminara <semi225599@gmail.com>
2014-09-14 02:11:32 -0400

    No cache

--- application.py
+++ application.py
@@ -3,17 +3,33 @@
 import os
 import requests
 from textblob import TextBlob
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file, jsonify,  make_response
+from functools import wraps, update_wrapper
+from datetime import datetime
+ 
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+    return update_wrapper(no_cache, view)
+
 
 application = Flask(__name__)
 
 API_URL = 'https://graph.facebook.com/v2.1'
 
 @application.route('/')
+@nocache
 def root():
     return application.send_static_file('index.html')
 
 @application.route('/sentiment', methods=['GET', 'POST'])
+@nocache
 def sent():
     if request.method == 'GET':
         text = request.args.get('message')
@@ -29,6 +45,7 @@ def sent():
     return send_file(img, mimetype='image/jpeg')
 
 @application.route('/fb/friends', methods=['POST'])
+@nocache
 def fbfriends():
     data = request.get_json()
     access_token = data.get('token')
