e0c6a25c17dd2f0c52dcb3fdab50b3dc6a7e1fcb
Kenny Ruddick <ruddick1@tcnj.edu>
2014-09-19 02:28:41 -0400

    Implemented JS compilation and optimization.

--- build/frontEndBuild.js
+++ build/frontEndBuild.js
@@ -10,10 +10,24 @@ var outDir = process.argv[2];
 var parser = new(less.Parser);
 compile.less.forEach(function(path) {
   var rawLess = fs.readFileSync('../' + path, { encoding: 'utf8' } );
-  
   parser.parse(rawLess, function (e, tree) {
     var out = outDir + '/' + path.replace('.less', '.css');
     mkdirp.sync(out.replace(/\/[^\/]*$/, ''));
     fs.writeFileSync(out, tree.toCSS({ compress: true }));
+    console.log('LESS compilation complete.');
   });
+});
+
+// Compile and optimize all of the JS files
+var config = {
+    baseUrl: '../static/js',
+    mainConfigFile: '../static/js/app.js',
+    name: 'app',
+    out: outDir + '/static/js/app.js'
+};
+
+requirejs.optimize(config, function (buildResponse) {
+    console.log('JS optimization complete.');
+}, function(err) {
+    console.log('JS optimization FAILED.');
 });
