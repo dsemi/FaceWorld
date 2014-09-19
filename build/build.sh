08a2832545e8a2fe10b9f954bf7075c27fdfffc4
Kenny Ruddick <ruddick1@tcnj.edu>
2014-09-19 02:46:57 -0400

    NPM installs the dependencies automatically now. Node vs Nodejs addressed.

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,11 @@
 
 OUT="../bin"
 
+echo Installing dependencies
+npm install
+
 echo Compiling LESS and JS
-node frontEndBuild.js $OUT
+node frontEndBuild.js $OUT || nodejs frontEndBuild.js $OUT
 echo Copying HTML
 cp ../static/index.html $OUT/static
 echo Copying resources
