a6a12e31e5074381f156cf0f171a2613bee0ddb1
Kenny Ruddick <ruddick1@tcnj.edu>
2014-09-19 02:01:35 -0400

    Fixed an issue so that the compiled LESS will now be minified.

--- build/frontEndBuild.js
+++ build/frontEndBuild.js
@@ -7,12 +7,13 @@ var fs = require('fs'),
 var outDir = process.argv[2];
 
 // Compile all of the LESS files
+var parser = new(less.Parser);
 compile.less.forEach(function(path) {
   var rawLess = fs.readFileSync('../' + path, { encoding: 'utf8' } );
   
-  less.render('.class { width: (1 + 1) }', function (e, css) {
+  parser.parse(rawLess, function (e, tree) {
     var out = outDir + '/' + path.replace('.less', '.css');
     mkdirp.sync(out.replace(/\/[^\/]*$/, ''));
-    fs.writeFileSync(out, css);
+    fs.writeFileSync(out, tree.toCSS({ compress: true }));
   });
 });
